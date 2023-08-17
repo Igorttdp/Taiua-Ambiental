5f944b0746097546c18330f3fc9a5cb872d395b2
Igorttdp <igorttdp.dev@email.com>
2023-08-16 23:09:55 -0300

    Build: add dependencies and config files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    formats: ["image/avif", "image/webp"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/pw/**",
+      },
+    ],
+  },
+  compiler: {
+    styledComponents: {
+      displayName: true,
+      ssr: true,
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
