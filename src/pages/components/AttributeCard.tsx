05d4d2217d84df0ee98f36d754791b81987381b8
Igorttdp <igorttdp.dev@email.com>
2023-08-21 00:22:31 -0300

    feat: add dataset to render component dinamically
    Also imports google font 'fira'

--- src/pages/components/AttributeCard.tsx
+++ src/pages/components/AttributeCard.tsx
@@ -1,8 +1,18 @@
+import Image from "next/image";
 import styled from "styled-components";
+import Check from "../../assets/check.svg";
+import ImageBtn from "../../assets/imagebtn.svg";
+import { Fira_Sans } from "next/font/google";
+import AttributeData from "@/interfaces/AttributesData";
 
-const AttributeCard = styled.article`
+const fira = Fira_Sans({
+  subsets: ["latin"],
+  weight: ["500", "600"],
+});
+
+const AttributeCardContainer = styled.article`
   width: 32rem;
-  min-height: 19rem;
+  min-height: 21rem;
   padding: 2.8rem 2rem;
   color: var(--white);
   background: var(--green-900);
@@ -34,9 +44,66 @@ const AttributeCard = styled.article`
     gap: 8px;
 
     > span {
-        font-size: 1.5rem;
+      font-size: 1.5rem;
     }
   }
 `;
 
-export default AttributeCard;
+const AttributeCards = () => {
+  const AttributesData: AttributeData[] = [
+    {
+      title: "Área de Convivência",
+      subtitle:
+        "Uma área ampla e aconchegante.Com camas, pufes, redes, Longe e muito mais.",
+    },
+    {
+      title: "Eventos culturais",
+      subtitle:
+        "Recebemos artistas voluntários, que fazem apresentações para hóspedes e moradores da Vila.",
+    },
+    {
+      title: "Vestiários",
+      subtitle: "Surpreenda-se com nossa limpeza e organização.",
+    },
+    {
+      title: "Cozinha equipada",
+      subtitle:
+        "Oferecemos uma cozinha completa e confortável com todos os equipamentos e utensílios para melhor atendê-los.",
+    },
+    {
+      title: "Sustentabilidade",
+      subtitle:
+        "Temos o cuidado de oferecer uma hospedagem que proporcione uma maior conscientização sobre a importância do bioma Cerrado.",
+    },
+    {
+      title: "Café da manhã",
+      subtitle:
+        "Temos parceria com uma cafeteria da Vila. Reserve com antecedência.",
+    },
+  ];
+
+  return (
+    <>
+      {AttributesData.map((el, i) => (
+        <AttributeCardContainer key={i}>
+          <div>
+            <Image width={37} height={37} src={Check} alt="ícone de checado" />
+            <h3>{el.title}</h3>
+          </div>
+          <p>{el.subtitle}</p>
+          <button className={fira.className}>
+            <Image
+              width={26}
+              height={26}
+              src={ImageBtn}
+              alt="ícone de imagem"
+            />
+            <span>Ver Imagens</span>
+          </button>
+        </AttributeCardContainer>
+      ))}
+    </>
+  );
+};
+
+export default AttributeCards;
