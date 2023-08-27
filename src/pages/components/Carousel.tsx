c96b76ad6cd71838933b607d87bd00872fe7d6bb
Igorttdp <igorttdp.dev@email.com>
2023-08-27 01:31:24 -0300

    feat: add Carousel component
    This component receives by props a object images that contains an array of images of type 'StaticImageData' and render then using the carousel components of 'coreui'

---
+++ src/pages/components/Carousel.tsx
@@ -0,0 +1,46 @@
+import { CCarousel, CCarouselItem } from "@coreui/react";
+import styled from "styled-components";
+import Image, { StaticImageData } from "next/image";
+
+interface ICarouselProps {
+  images: StaticImageData[];
+}
+
+const StyledCCarousel = styled(CCarousel)`
+  width: 38rem;
+  position: relative;
+
+  @media (max-width: 767.98px) {
+    width: 80vw;
+  }
+
+  @media (max-width: 425px) {
+    width: 90vw;
+  }
+
+  img {
+    border-radius: 1rem;
+    width: 38rem;
+    height: 46rem;
+    object-fit: cover;
+
+    @media (max-width: 767.98px) {
+      width: inherit;
+      height: 75vh;
+    }
+  }
+`;
+
+const Carousel = ({ images }: ICarouselProps) => {
+  return (
+    <StyledCCarousel controls transition="crossfade">
+      {images.map((el) => (
+        <CCarouselItem key={el.src}>
+          <Image src={el} alt="Imagem do Quarto" placeholder="blur" />
+        </CCarouselItem>
+      ))}
+    </StyledCCarousel>
+  );
+};
+
+export default Carousel;
