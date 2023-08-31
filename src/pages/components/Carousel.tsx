8a98e4da084ab48c18201f42583fd240b47fe73b
Igorttdp <igorttdp.dev@email.com>
2023-08-31 02:13:56 -0300

    feat: call new 'CarouselThumb' component
    Also, declare states to manage 'CarouselThumb' styles

--- src/pages/components/Carousel.tsx
+++ src/pages/components/Carousel.tsx
@@ -1,9 +1,16 @@
 import { CCarousel, CCarouselItem } from "@coreui/react";
 import styled from "styled-components";
 import Image, { StaticImageData } from "next/image";
+import CarouselThumb from "./CarouselThumb";
+import { useState } from "react";
 
 interface ICarouselProps {
   images: Array<StaticImageData>;
+}
+
+export interface IThumbProps {
+  thumb: StaticImageData;
+  thumbIndex: number;
 }
 
 const StyledCCarousel = styled(CCarousel)`
@@ -32,13 +39,55 @@ const StyledCCarousel = styled(CCarousel)`
 `;
 
 const Carousel = ({ images }: ICarouselProps) => {
+  const [activeThumb, setActiveThumb] = useState(0);
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const chunkArray = () => {
+    const result: IThumbProps[][] = [];
+    for (let i = 0; i < images.length; i += 4) {
+      const chunk = images.slice(i, i + 4).map((image, index) => ({
+        thumb: image,
+        thumbIndex: i + index,
+      }));
+      result.push(chunk);
+    }
+    return result;
+  };
+
+  const thumbs = chunkArray();
+
+  const onSlideCarousel = (active: number) => {
+
+    setActiveIndex(active);
+
+    if (active % 4 === 0) {
+      setActiveThumb(active / 4);
+    }
+  };
+
   return (
-    <StyledCCarousel controls transition="crossfade">
-      {images && images.map((el) => (
-        <CCarouselItem key={el.src}>
-          <Image src={el} width={800} height={800} alt="Imagem do Quarto" placeholder="blur" />
-        </CCarouselItem>
-      ))}
+    <StyledCCarousel controls transition="crossfade" onSlide={onSlideCarousel}>
+      {images &&
+        images.map((el) => (
+          <CCarouselItem key={el.src}>
+            <Image
+              src={el}
+              width={800}
+              height={800}
+              alt="Imagem do Quarto"
+              placeholder="blur"
+            />
+          </CCarouselItem>
+        ))}
+      {thumbs &&
+        thumbs.map((thumb, i) => (
+          <CarouselThumb
+            key={`thumb${i}`}
+            thumb={thumb}
+            visible={i === activeThumb ? true : false}
+            activeIndex={activeIndex}
+          />
+        ))}
     </StyledCCarousel>
   );
 };
