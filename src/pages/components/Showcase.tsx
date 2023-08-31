508d396313516afff9f62c9d68f360be90d65e5f
Igorttdp <igorttdp.dev@email.com>
2023-08-31 02:10:42 -0300

    style: add styles for 'flutuant' background div

--- src/pages/components/Showcase.tsx
+++ src/pages/components/Showcase.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
-const Showcase = styled.div`
+interface IShowcaseProps {
+  $filter: string;
+}
+
+const Showcase = styled.div<IShowcaseProps>`
   position: relative;
   color: white;
   min-height: 100vh;
@@ -11,12 +15,43 @@ const Showcase = styled.div`
   justify-content: center;
   gap: 4rem;
 
+  > div {
+    background-image: linear-gradient(
+      180deg,
+      rgba(61, 84, 48, 0.03) 8.85%,
+      #47702f 27.08%,
+      #FFF 100%
+    );
+    position: absolute;
+    width: 100%;
+    min-height: 100%;
+    bottom: -60%;
+    z-index: 2;
+  }
+
+  > video {
+    object-fit: fill;
+    position: absolute;
+
+    &:first-of-type {
+      z-index: 1;
+      height: 100vh;
+    }
+
+    &:last-of-type {
+      width: 100%;
+      filter: ${({ $filter }) => $filter};
+    }
+  }
+
   h1 {
     text-align: center;
 
     a > img {
+      position: relative;
       max-width: 30.9rem;
       max-height: 30.9rem;
+      z-index: 3;
 
       @media (max-width: 840px) {
         width: 75%;
