98f4ec2834af5746793570fdab48f81d45ae92e3
Igorttdp <igorttdp.dev@email.com>
2023-08-31 02:08:41 -0300

    build: remove some config option

--- next.config.js
+++ next.config.js
@@ -3,13 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        pathname: "/pw/**",
-      },
-    ],
   },
   compiler: {
     styledComponents: {
